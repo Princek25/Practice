ce1f277a75d88f874154fc045a52afb3b10f06b1
Prince <princek2530@gmail.com>
2022-09-30 20:08:39 +0530

    Added: Check Whether a Given Number is Prime or Not

--- src/com/bridgelabz/basics/PrimeNumber.java
+++ src/com/bridgelabz/basics/PrimeNumber.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-
+        int c=0; // division count  // Declared + Initialized a variable
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a Number");
+        int a = sc.nextInt();
+        for (int i=1;i<=a;i++){
+            if (a%i==0) {
+                c++;  // Increment
+            }
+        }
+        if (c==2){
+            System.out.println(a + " is a Prime Number");
+        }
+        else System.out.println(a + " is not a Prime Number");
     }
 }
