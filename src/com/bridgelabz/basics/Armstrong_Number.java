d391ac7d99c5aefd05f6c563ffffc16c0eb8f481
Prince <princek2530@gmail.com>
2022-09-30 20:10:49 +0530

    Added: Check whether or Not the Number is an Armstrong Number

--- src/com/bridgelabz/basics/Armstrong_Number.java
+++ src/com/bridgelabz/basics/Armstrong_Number.java
@@ -1,2 +1,10 @@
-package com.bridgelabz.basics;public class Armstrong_Number {
+package com.bridgelabz.basics;
+
+// Armstrong number is the sum of the cubes of the digits of any number
+// which is equal to the number itself
+
+public class Armstrong_Number {
+    public static void main(String[] args) {
+
+    }
 }
