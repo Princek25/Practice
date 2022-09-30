f1994ab3c34844f8e76e28e03cf2f0afdeb31af9
Prince <princek2530@gmail.com>
2022-09-30 20:16:06 +0530

    Added: Check if a Number is Positive or Negative

--- src/com/bridgelabz/basics/Positive_Or_Negative_Number.java
+++ src/com/bridgelabz/basics/Positive_Or_Negative_Number.java
@@ -4,8 +4,8 @@ import java.util.Scanner;   // importing a scanner class
 public class Positive_Or_Negative_Number {
     public static void main(String[] args) {
         System.out.println("Enter a number ");  // Taking input from the user
-        Scanner sc = new Scanner(System.in);   // creating an object of scanner class (sc)
-        int a = sc.nextInt();                 // creating a variable (a) of int type and calling the object sc
+        Scanner sc = new Scanner(System.in);   // Creating an object of scanner class (sc)
+        int a = sc.nextInt();                 // Creating a variable (a) of int type and calling the object sc
         if (a>=1){
             System.out.println("It is a positive number");
         }
