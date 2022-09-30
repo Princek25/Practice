2d51f8cbf725a6f9d2b99c5d4b22d3cde4381479
Prince <princek2530@gmail.com>
2022-09-30 20:34:33 +0530

    Added: Check Whether or Not the Year is a Leap Year

--- src/com/bridgelabz/basics/Leap_Year_Or_Not.java
+++ src/com/bridgelabz/basics/Leap_Year_Or_Not.java
@@ -2,7 +2,7 @@ package com.bridgelabz.basics;
 import java.util.Scanner;
 
 // 1. A leap year can be divisible by 4 and 100 both.
-// 2. A leap year can be divisible by 4 only
+// 2. A leap year can be divisible by 4 only.
 // 3. but a leap year cannot be divisible by 100 only.
 
 public class Leap_Year_Or_Not {
