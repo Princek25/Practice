237cc9a0548c6ca9d05f0a7ea7e0e2c858b38d7d
Prince <princek2530@gmail.com>
2022-10-02 11:53:13 +0530

    Added: Find the Greatest Number Among Three Numbers

--- src/com/bridgelabz/basics/Greatest_Of_Three_Numbers.java
+++ src/com/bridgelabz/basics/Greatest_Of_Three_Numbers.java
@@ -1,20 +1,26 @@
 package com.bridgelabz.basics;
-import java.util.Scanner;  // Importing a scanner class
+import java.util.Scanner;
+
+// Greatest number among three numbers
+// Programing logic = a>b, a>c and b>a, b>c and c>a, c>a
 
 public class Greatest_Of_Three_Numbers {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);        // Creating an object of scanner class (sc)
-        System.out.println("Input First Number");  // Taking input from the user
-        int a = sc.nextInt();                     // Creating a variable (a) of int type and calling the object sc
+    static void greatestNumber(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input First Number");
+        int a = sc.nextInt();      // Taking user input of first number
         System.out.println("Input Second Number");
-        int b = sc.nextInt();
+        int b = sc.nextInt();      // Taking user input of second number
         System.out.println("Input Third Number");
-        int c = sc.nextInt();
+        int c = sc.nextInt();      // Taking user input of third number
         if (a>b & a>c){
             System.out.println(a + " " + "is greatest");
         } else if (b>a & b>c) {
             System.out.println(b + " " + "is greatest");
         }
         else System.out.println(c + " " + "is greatest");
+    }
+    public static void main(String[] args) {
+        greatestNumber();
     }
 }
