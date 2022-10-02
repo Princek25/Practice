6ff3d3b71a74418f3187b5db4f1ae89b6c558f77
Prince <princek2530@gmail.com>
2022-10-02 12:37:27 +0530

    Added: Find the Sum of the Digits of a Number

--- src/com/bridgelabz/basics/Sum_Of_Digits.java
+++ src/com/bridgelabz/basics/Sum_Of_Digits.java
@@ -1,17 +1,23 @@
 package com.bridgelabz.basics;
 import java.util.Scanner;
 
+// Sum of digits:
+// Programing logic = let a user input = 123, 123%10 = (3), 123/10 = 12, 12%10 = (2), 12/10 = (1)
+
 public class Sum_Of_Digits {
-    public static void main(String[] args) {
-        int sum = 0;    // Taking a variable sum of integer type initiate with zero
-        Scanner sc = new Scanner(System.in);        // Scanner Object sc
-        System.out.println("Input a Number");      // Taking a input from the user
-        int a = sc.nextInt();
-        while (a>0){                     // Using while loop a should be greater than 0
-            int b = a%10;              // Taking another variable b and the remainder of a/10 stored in it.
-            sum=sum+b;               // Another variable sum to add with b
-            a=a/10;
+    static void sumOfDigits(){
+        int a,sum = 0;    // Taking two variable a = user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a Number");
+        a = sc.nextInt();   // Taking user input stored in variable (a) let a = 123.
+        while (a>0){        // 123>0 True            // a = 12>0 True       // a = 1>0 True
+            int b = a%10;   // b = 123%10 = 3 = rem  // b = 12%10 = 2 = rem // b = 1%10 = 1
+            sum=sum+b;      // sum = 0+3 = 3         // sum = 3+2 = 5       // sum = 5+1 = 6
+            a=a/10;         // a = 123/10 = 12       // a = 12/10 = 1       // a = 1/10 = 0
         }
         System.out.println("Sum of digits of the number is:- " + sum);
+    }
+    public static void main(String[] args) {
+        sumOfDigits();
     }
 }
