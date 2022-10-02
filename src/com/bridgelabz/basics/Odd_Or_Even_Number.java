68b3dcdb8a61640bb41997f93472751a39304497
Prince <princek2530@gmail.com>
2022-10-02 12:19:41 +0530

    Added: Check Whether a Number is Even or Odd

--- src/com/bridgelabz/basics/Odd_Or_Even_Number.java
+++ src/com/bridgelabz/basics/Odd_Or_Even_Number.java
@@ -1,15 +1,21 @@
 package com.bridgelabz.basics;
-import java.util.Scanner;     // importing a scanner class
+import java.util.Scanner;
+
+// Odd and Even Numbers
+// Programing logic = take a user input put in a variable (a) then if a%2=0 then it will be even number
 
 public class Odd_Or_Even_Number {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);     // Creating an object of scanner class (sc)
-        System.out.println("Enter a number");   // Taking input from the user
-        int a = sc.nextInt();                  // Creating a variable (a) of int type and calling the object sc
-        if (a%2==0){                          // If any number divided by 2 with 0 remainder then it will be even no. otherwise it will be odd no.
+    static void oddEven(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int a = sc.nextInt();      // Taking user input stored in a variable
+        if (a%2==0){              // Even no. always gives remainder 0 when is divided by 2.
             System.out.println("It is even number");
         }
         else System.out.println("It is odd number");
+    }
+    public static void main(String[] args) {
+        oddEven();
 
     }
 }
