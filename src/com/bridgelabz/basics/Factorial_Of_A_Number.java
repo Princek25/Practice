805fade325e655fe55f7efe285ea7abfa5ad858c
Prince <princek2530@gmail.com>
2022-10-02 10:11:45 +0530

    Added: Find the Factorial of a Number

--- src/com/bridgelabz/basics/Factorial_Of_A_Number.java
+++ src/com/bridgelabz/basics/Factorial_Of_A_Number.java
@@ -1,16 +1,20 @@
 package com.bridgelabz.basics;
 import java.util.Scanner;
 
+// Factorial of a number eg:- 6 = 1x2x3x4x5x6 = 720
+
 public class Factorial_Of_A_Number {
-    public static void main(String[] args) {
-        int x=1;     // x = factorial
+    static void factorialNumber(){
+        int x=1;               // x = 1 multiply x with any number become that number
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number:- ");
         int a = sc.nextInt();  // Taking input from the user
         for(int i=1;i<=a;i++){ // Using for loop to print 1 to the user input number
-            x=x*i;             // Multiply all the number
+            x=x*i;             // Multiply all the number and stored in x container
         }
         System.out.println("Factorial of "+ a + " is: " + x);
-
+    }
+    public static void main(String[] args) {
+        factorialNumber();
     }
 }
