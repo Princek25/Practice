f79833a63d60ba835902954db9a9a06b7921749d
Prince <princek2530@gmail.com>
2022-10-02 11:59:32 +0530

    Added: Find the Greatest of the Two Numbers

--- src/com/bridgelabz/basics/Greatest_Of_Two_Numbers.java
+++ src/com/bridgelabz/basics/Greatest_Of_Two_Numbers.java
@@ -1,17 +1,22 @@
 package com.bridgelabz.basics;
-import java.util.Scanner;     // Importing a scanner class
+import java.util.Scanner;
+
+// Greatest of two numbers
+// Programing logic = if a>b then a is greatest otherwise b is greatest
 
 public class Greatest_Of_Two_Numbers {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);    // Creating an object of scanner class (sc)
-        System.out.println("Input a number");  // Taking input from the user
-        int a = sc.nextInt();                 // Creating a variable (a) of int type and calling the object sc
+    static void greatestNumber(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number");
+        int a = sc.nextInt();         // Taking user input of first number.
         System.out.println("Input another number");
-        int b = sc.nextInt();
+        int b = sc.nextInt();         // Taking user input of second number.
         if (a>b){
             System.out.println(a+ " " + "is greatest");
         }
         else System.out.println(b+ " " + "is greatest");
-
+    }
+    public static void main(String[] args) {
+        greatestNumber();
     }
 }
