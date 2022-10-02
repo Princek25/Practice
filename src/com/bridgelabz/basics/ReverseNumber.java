d2b6ba79f14cf65a6f5ad58331bce3fea4e9613f
Prince <princek2530@gmail.com>
2022-10-02 10:31:59 +0530

    Added: Find the Reverse of a Number

--- src/com/bridgelabz/basics/ReverseNumber.java
+++ src/com/bridgelabz/basics/ReverseNumber.java
@@ -1,18 +1,22 @@
 package com.bridgelabz.basics;
 import java.util.Scanner;
 
+// Input = 123 Output = 321
+
 public class ReverseNumber {
-    public static void main(String[] args) {
-        int a,b;     // Declaring a and b variable of integer type.
-        int c=0;    // Declaring another variable c initializing with 0.
+    static void reverseNumber(){
+        int a,b;     // Declaring (a = user input and b = remainder) variables of integer type.
         Scanner sc = new Scanner(System.in);
         System.out.println("Input a number");
         a=sc.nextInt();
-        while (a>0){      // condition of while loop
-            b = a%10;     // b container store the value of remainder by dividing user input number by 10.
-            a = a/10;     // a store the quotient value of a/10.
-            c = (c*10)+b; // C = 0*10 + 6 = 6 again C = 10*6 + 4 = 64
+        System.out.println("The reverse of "+ a + " is:- ");
+        while (a>0){      // a = user input lets = 123, a>0 yes condition true // a = 12 > 0 condition true
+            b = a%10;     // b = 123%10 = 3 = remainder and print 3.           // b = 12%10 = 2 = remainder and print 2.
+            a = a/10;     // a = 12 now because 3 remove. // now again a>0    //  a = 1 this time repeat one more time
+            System.out.print(b);
         }
-        System.out.println("Reverse of the number is:- " + c);
+    }
+    public static void main(String[] args) {
+        reverseNumber();
     }
 }
