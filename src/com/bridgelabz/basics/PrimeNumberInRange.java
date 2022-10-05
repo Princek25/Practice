cd4a07bdf05b51f56ee28572269b7477bdb7e27e
Prince <princek2530@gmail.com>
2022-10-05 13:18:36 +0530

    Added: Find all the Prime Numbers in a Given Interval

--- src/com/bridgelabz/basics/PrimeNumberInRange.java
+++ src/com/bridgelabz/basics/PrimeNumberInRange.java
@@ -2,23 +2,20 @@ package com.bridgelabz.basics;
 import java.util.Scanner;
 
 public class PrimeNumberInRange {
-    public static void checkPrimeNumberInRange(){
-        int a,b,i,j;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input Initial Number");  // let = 5
-        a=sc.nextInt();
-        System.out.println("Input Final Number");   // let 25
-        b=sc.nextInt();
-        for (i=a;i<=b;i++){            // i=5; i<=25 true
-            for (j=2;j<i;j++){         //
-                if (i%j==0) break;     // j will not break in 5, 7, 11, 13, 17,23 value stored in j. but it will break in 6,8...
-            }
-            if (i==j){                 // if i=j then print j
-                System.out.print(j + " ");
-            }
+    public static void checkPrimeNumberInRange(int initialN, int finalN){
+        for (int i=initialN;i<=finalN;i++){
+            boolean isPrime = PrimeNumber.checkPrimeNumber(i);
+            if (isPrime)
+            System.out.print(i + " ");
         }
     }
     public static void main(String[] args) {
-        checkPrimeNumberInRange();
+        System.out.println("Welcome to the 'Prime number within a given range' Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Initial Number");
+        int a=sc.nextInt();
+        System.out.println("Input Final Number");
+        int b=sc.nextInt();
+        checkPrimeNumberInRange(a,b);
     }
 }
