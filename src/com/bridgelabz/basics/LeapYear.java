66d7462c2db8e83f9010d4961d279320c0955b9e
Prince <princek2530@gmail.com>
2022-10-05 10:53:11 +0530

    Added: Check Whether or Not the Year is a Leap Year

--- src/com/bridgelabz/basics/LeapYear.java
+++ src/com/bridgelabz/basics/LeapYear.java
@@ -5,16 +5,13 @@ import java.util.Scanner;
 // 1. A leap year is divisible by 4 and a non leap year is not divisible by 4.
 // 2. Problem: 1900 is a non leap year which is divisible by 4.
 // 3. Then for century leap year, is divisible by 400.
-// 4. A leap year cannot be divisible by 100.
+// 4. A century leap year cannot be divisible by 100 only.
 
-public class Leap_Year_Or_Not {
-    static void leapYear(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a year:- ");
-        int year = sc.nextInt();
-        if (year%400==0){          // A leap year is divisible by 400 means remainder is 0.
+public class LeapYear {
+    static void checkLeapYear(int year){
+        if (year%400==0){          // A century leap year is divisible by 400 means remainder is 0.
             System.out.println(year + " is a leap year");
-        } else if (year%100==0) {  // A non leap is divisible by 100 means remainder is 0.
+        } else if (year%100==0) {  // A century leap year is not divisible by 100 only it should be 400.
             System.out.println(year + " is not a leap year");
         } else if (year%4==0) {    // A leap year is divisible by 4 means remainder is 0.
             System.out.println(year + " is a leap year");
@@ -22,6 +19,10 @@ public class Leap_Year_Or_Not {
         else System.out.println(year + " is not a leap year");
     }
     public static void main(String[] args) {
-        leapYear();
+        System.out.println("Welcome to the 'leap year or not' program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year:- ");
+        int a = sc.nextInt();
+        checkLeapYear(a);
     }
 }
