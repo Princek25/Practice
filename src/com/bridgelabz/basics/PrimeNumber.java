4dd0b3b2d836421a112ea159466975ad7950cbb4
Prince <princek2530@gmail.com>
2022-10-05 11:13:02 +0530

    Added: Check Whether a Given Number is Prime or Not

--- src/com/bridgelabz/basics/PrimeNumber.java
+++ src/com/bridgelabz/basics/PrimeNumber.java
@@ -4,12 +4,14 @@ package com.bridgelabz.basics;
 // Programing Logic = Apart from 1 and itself divides the number with all its divisors,
 // If it is divisible then enter inside isPrime = false and break, if it not divisible then it goes to isPrime = true
 
+import java.util.Scanner;
+
 public class PrimeNumber {
     static boolean checkPrimeNumber(int num) {
-        boolean isPrime = true;
-        for (int i=2;i<num;i++){
-            if (num%i==0){
-                isPrime = false;
+        boolean isPrime = true;   // isPrime store true let num = 7
+        for (int i=2;i<num;i++){  // i = 2;i<7;true(why not 1 or <= , because it gives the remainder 0)
+            if (num%i==0){        // 7%2==0 false it does not enter inside,then post increment goes to 6 and none of them can enter inside
+                isPrime = false;  // only composite number enter inside
                 break;
             }
         }
@@ -17,8 +19,12 @@ public class PrimeNumber {
     }
 
     public static void main(String[] args) {
-        boolean b = checkPrimeNumber(12);
-        if (b) System.out.println("It is a Prime Number");
-        else System.out.println("It is not a Prime Number");
+        System.out.println("Welcome to the 'check prime number or not' program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number");
+        int a = sc.nextInt();
+        boolean b = checkPrimeNumber(a);
+        if (b) System.out.println(a+ " is a Prime Number");
+        else System.out.println(a + " is not a Prime Number");
     }
 }
