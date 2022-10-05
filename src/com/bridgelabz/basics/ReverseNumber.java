791746aee1513196d7f8ef0286a4edf899733dae
Prince <princek2530@gmail.com>
2022-10-05 13:38:31 +0530

    Added: Reverse a given number

--- src/com/bridgelabz/basics/ReverseNumber.java
+++ src/com/bridgelabz/basics/ReverseNumber.java
@@ -1,22 +1,20 @@
 package com.bridgelabz.basics;
 import java.util.Scanner;
 
-// Input = 123 Output = 321
-
 public class ReverseNumber {
-    static void reverseNumber(){
-        int a,b;     // Declaring (a = user input and b = remainder) variables of integer type.
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input a number");
-        a=sc.nextInt();
-        System.out.println("The reverse of "+ a + " is:- ");
-        while (a>0){      // a = user input lets = 123, a>0 yes condition true // a = 12 > 0 condition true
-            b = a%10;     // b = 123%10 = 3 = remainder and print 3.           // b = 12%10 = 2 = remainder and print 2.
-            a = a/10;     // a = 12 now because 3 remove. // now again a>0    //  a = 1 this time repeat one more time
+    public static void reverseNumber(int a){
+        while (a>0){         // let user input = 123 // a = 12 > 0 true  // a = 1 > 0 true
+           int b = a%10;     // b = 123%10 = 3       // b = 12%10 = 2    // b = 2/10 = 0
+            a = a/10;        // a = 12/10 = 12       // a = 12/10 = 1    // a = 1/10 = 0
             System.out.print(b);
         }
     }
     public static void main(String[] args) {
-        reverseNumber();
+        System.out.println("Welcome to the 'Reverse of a Number' Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number");
+        int a=sc.nextInt();
+        System.out.println("The reverse of "+ a + " is:- ");
+        reverseNumber(a);
     }
 }
