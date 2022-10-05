77d7b8a1ae21c2ab7400e3491e6144c06fe17454
Prince <princek2530@gmail.com>
2022-10-05 15:27:17 +0530

    Added: Check Whether or Not the Number is a Perfect Number

--- src/com/bridgelabz/basics/PerfectNumber.java
+++ src/com/bridgelabz/basics/PerfectNumber.java
@@ -5,14 +5,11 @@ package com.bridgelabz.basics;
 
 import java.util.Scanner;
 
-public class Perfect_Number {
-    static void perfectNumber(){
-        int n, sum=0;         // Taking two variables n(user input) and sum=0(to store the value of i outside the loop.
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input a number");
-        n= sc.nextInt();       // Taking user input let 6.
+public class PerfectNumber {
+    public static void checkPerfectNumber(int n){
+        int sum=0;             // let user input = 6 = n
         for (int i=1;i<n;i++){ // i=1; i<6 true
-            if (n%i==0){       // 6%1=0 true
+            if (n%i==0){       // 6%1==0 true
                 sum = sum + i; // sum = 0+1 =1  // The whole process repeat, sum become = 1 + 2 + 3 = 6
             }
         }
@@ -23,6 +20,10 @@ public class Perfect_Number {
     }
 
     public static void main(String[] args) {
-        perfectNumber();
+        System.out.println("Welcome to the 'Perfect Number' Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number");
+        int n= sc.nextInt();
+        checkPerfectNumber(n);
     }
 }
