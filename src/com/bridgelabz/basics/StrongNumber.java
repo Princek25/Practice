880dffda677d62d2ad13a8a8bcbe239d5f635d16
Prince <princek2530@gmail.com>
2022-10-05 17:58:37 +0530

    Added: Check Whether or Not the Number is a Strong Number

--- src/com/bridgelabz/basics/StrongNumber.java
+++ src/com/bridgelabz/basics/StrongNumber.java
@@ -1,2 +1,30 @@
-package com.bridgelabz.basics;public class StrongNumber {
+package com.bridgelabz.basics;
+import java.util.Scanner;
+
+// Strong Number Logic:-
+// 145 =1! + 4! + 5! = 1 + 24 + 120 = 145
+
+public class StrongNumber {
+    public static void checkStrongNumber(int n){
+        int sum = 0,n1=n; // n1 is copy of n for comparison
+        while (n!=0){
+            int r = n%10;  // for last digit
+            sum = sum + Factorial.factorialNumber(r);
+            n=n/10;   // it will remove last digit
+        }
+        if (sum==n1){
+            System.out.println("Strong Number");
+        }
+        else {
+            System.out.println("Not a Strong Number");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the 'Strong Number' Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a Number");
+        int n = sc.nextInt();
+        checkStrongNumber(n);
+    }
 }
