868b0e850fdb1769908ea0b16875145c20b10341
Prince <princek2530@gmail.com>
2022-10-05 17:56:58 +0530

    Added: Factorial of a Number

--- src/com/bridgelabz/basics/Factorial.java
+++ src/com/bridgelabz/basics/Factorial.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 // Factorial of a number eg:- 6 = 1x2x3x4x5x6 = 720
 
 public class Factorial {
-    public static void factorialNumber(int a){
+    public static int factorialNumber(int a){
         int x=1;
         for(int i=1;i<=a;i++){ // Using for loop to print 1 to the user input number
             x=x*i;             // Multiply all the number and stored in x container
         }
-        System.out.println("Factorial of "+ a + " is: " + x);
+        System.out.println(x);
+        return x;
     }
     public static void main(String[] args) {
         System.out.println("Welcome to the 'Factorial of a Number' Program");
