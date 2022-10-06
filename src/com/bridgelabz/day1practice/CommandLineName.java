e4f0b53f5e081fcf4af26ac9e3f122c9ffc26d41
Prince <princek2530@gmail.com>
2022-10-06 13:37:56 +0530

    Added: To get the name using the command line

---
+++ src/com/bridgelabz/day1practice/CommandLineName.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.day1practice;
+
+// 1.5 To get the name using the command line.
+
+public class CommandLineName {
+    public static void main(String[] args) {
+        System.out.println(args [0]);
+    }
+}
