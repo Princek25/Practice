516bea74ee2a172cfbd5db6826b29ed05e4680cd
Prince <princek2530@gmail.com>
2022-10-06 11:29:29 +0530

    Added: Display A Message

---
+++ src/com/bridgelabz/day1practice/DisplayMessage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day1practice;
+
+public class DisplayMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Practice Problem of Day 1");
+    }
+}
