fadb6faeb7ff849274935345ac7fc87282b89287
Prince <princek2530@gmail.com>
2022-10-10 21:10:56 +0530

    Added: Java Program to print the elements of an array

---
+++ src/com/bridgelabz/day3array/ArrayElements.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.day3array;
+
+public class ArrayElements {
+
+    public static void main(String[] args) {
+        int [] a = {10,20,30,40,50,60,70,80};
+
+        System.out.println("Printing elements of array");
+        for (int i=0; i< a.length; i++){
+            System.out.println(a[i]);
+        }
+
+    }
+}
