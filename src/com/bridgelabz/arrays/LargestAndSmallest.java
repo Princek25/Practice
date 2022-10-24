292af7923a9edb4faf60a1219fc1997e43689cbf
Prince <princek2530@gmail.com>
2022-10-24 12:45:04 +0530

    Added: Find the Smallest and largest element in an array

--- src/com/bridgelabz/arrays/LargestAndSmallest.java
+++ src/com/bridgelabz/arrays/LargestAndSmallest.java
@@ -1,2 +1,19 @@
-package com.bridgelabz.arrays;public class LargestAndSmallest {
+package com.bridgelabz.arrays;
+
+public class LargestAndSmallest {
+    public static void main(String[] args) {
+        int arr [] = {53,54,24,35,85,45,77};
+        int max = arr[0]; // assume
+        int min = arr[0]; // assume
+        for (int i=0;i< arr.length;i++){
+            if (max<arr[i]){
+                max=arr[i];
+            }
+            if (min>arr[i]){
+                min=arr[i];
+            }
+        }
+        System.out.println(max + " is largest element of the array");
+        System.out.println(min + " is smallest element of the array");
+    }
 }
