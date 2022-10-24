44179d1e7fac9f30d9e9b039e8de86b0fc886e02
Prince <princek2530@gmail.com>
2022-10-24 12:32:14 +0530

    Added: Find Smallest Element in an Array

---
+++ src/com/bridgelabz/arrays/SmallestArray.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.arrays;
+
+public class SmallestArray {
+    public static void main(String[] args) {
+        int arr [] = {37,42,25,35,45,78,21,34};
+
+        int min = arr[0]; //assume
+
+        for (int i=0;i< arr.length;i++){
+            if (min>arr[i]){
+                min=arr[i];  
+            }
+        }
+        System.out.println(min + " is the smallest element in the array");
+    }
+}
